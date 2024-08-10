ba19de4814bc600472d4218aff2dafd75bc475b9
jmkjoel004 <137641654+jmkjoel004@users.noreply.github.com>
2024-08-09 20:05:15 -0400

    Add files via upload

---
+++ players/players/urls.py
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('members.urls')),
+    path('admin/', admin.site.urls),
+=======
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('members.urls')),
+    path('admin/', admin.site.urls),
+>>>>>>> origin/main
+]
